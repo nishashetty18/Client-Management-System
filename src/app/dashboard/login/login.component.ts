a2c06a881a9f4a6b4d2cccc715020433bc3d540c
nisha <shettynishi55@gmail.com>
2024-02-10 19:25:17 +0530

    login uupdated

--- src/app/dashboard/login/login.component.ts
+++ src/app/dashboard/login/login.component.ts
@@ -9,46 +9,46 @@ import { ClientService } from 'src/app/services/client.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username:string="";
-  password:string="";
-  message:string="";
-  clnt:Client[]=[];
-  constructor(private clientsystem:ClientService,private router:Router){
+  username: string = "";
+  password: string = "";
+  message: string = "";
+  clnt: Client[] = [];
+  constructor(private clientsystem: ClientService, private router: Router) {
     sessionStorage.clear();
   }
 
-  usercheck()
-  {
-    if(this.username=="admin"&&this.password=="admin@123"){
+  usercheck() {
+    if (this.username == "admin" && this.password == "admin@123") {
       this.router.navigate(['/home']);
     }
-    else
-    this.clientsystem.getallclient().subscribe((data:any)=>{
-      this.clnt=data;
-    });
-    var uchk=false;
-    for(let i=0;i<this.clnt.length;i++)
-    {
-      if(this.clnt[i].email==this.username&& this.clnt[i].password==this.password)
-      {
-      sessionStorage.setItem('user',this.clnt[i].name);
-      sessionStorage.setItem('id',this.clnt[i].id.toString())
-      uchk=true;
-      break;
-      }   
-    }
-    if(uchk==true)
-    {
-      this.router.navigate(['/singleproject']);
-    }
-    else[]
-  
-      if(this.username==""||this.password==""){
-        this.message="Please enter username/password"
+    else{
+      this.clientsystem.getallclient().subscribe((data: any) => {
+        this.clnt = data;
+      
+      var uchk = false;
+      for (let i = 0; i < this.clnt.length; i++) {
+        if (this.clnt[i].email == this.username && this.clnt[i].password == this.password) {
+          sessionStorage.setItem('user', this.clnt[i].name);
+          sessionStorage.setItem('id', this.clnt[i].id.toString())
+          uchk = true;
+          break;
+        }
       }
-    else{ this.message="Please enter correct username/password"}
-    
-    
+      if (uchk == true) {
+        this.router.navigate(['/singleproject']);
+      }
+      else {
+
+        if (this.username == "" || this.password == "") {
+          this.message = "Please enter username/password"
+        }
+        else 
+        { 
+          this.message = "Please enter correct username/password" 
+        }
+      }
+    });
+    }
   }
 }
 
